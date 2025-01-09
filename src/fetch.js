61a52e5c06286290847311ae0ff6537c2057995a
JoYoungHyun <joyounghyun550@gmail.com>
2025-01-09 14:00:15 +0900

    123

--- src/fetch.js
+++ src/fetch.js
@@ -1,4 +1,4 @@
-const options = {
+export const options = {
   method: "GET",
   headers: {
     accept: "application/json",
@@ -7,10 +7,45 @@ const options = {
   },
 };
 
-fetch(
-  `https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1`,
-  options
-)
-  .then((res) => res.json())
-  .then((res) => console.log(res))
-  .catch((err) => console.error(err));
+export class HttpError extends Error {
+  constructor(response) {
+    super(`${response.status} for ${response.url}`);
+    this.name = "HttpError";
+    this.response = response;
+  }
+}
+
+// loadJson함수는 url을 입력받아 fetch함수를 호출해주고 그 통신이 성공했을때(statusCode 200), 결과를 반환해주는 함수입니다.
+// 만약 통신이 실패하는 경우 위에 작성한 에러 객체를 반환해줍니다.
+export async function loadJson(url) {
+  // fetch함수는 네트워크 통신 프로미스를 반환하는 대표적인 함수
+  const response = await fetch(url, options);
+  if (response.status == 200) {
+    return response.json();
+  } else {
+    throw new HttpError(response);
+  }
+}
+
+export async function Movie() {
+  while (true) {
+    try {
+      res = await loadJson(
+        `https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1`
+      );
+      break;
+    } catch (err) {
+      if (err instanceof HttpError && err.response.status == 404) {
+        alert("무언가 에러가 발생했군요!");
+      } else {
+        throw err;
+      }
+    }
+  }
+  const data = res.results;
+  console.log(data[0].title, data[0].backdrop_path, data[0].vote_average);
+
+  return data;
+}
+
+Movie();
--- src/main.js
+++ src/main.js
@@ -1,16 +1,21 @@
+import { Movie, options, HttpError, loadJson } from "./fetch.js";
+
+let movieData = Movie;
+
+console.log(movieData[0].title);
 const movieContainer = document.querySelector(".movie-container");
 
-movies.forEach((movie, idx) => {
+movieData.forEach((movie, idx) => {
   const movieDiv = document.createElement("div");
   movieDiv.className = "movie";
 
   movieDiv.innerHTML = `
             <div class="photo movie_content" id="movie_content_${idx}">
-                <img src="${movie.backgroundImage}" alt="${movie.title}">
+                <img src="https://image.tmdb.org/t/p/w500${movie["poster_path"]}">
             </div>            
             <div class="movie_content">
-            <h5 class='title'>${movie.title}</h5>
-            <span class='rating'>${movie.rating}</span>
+            <h5 class='title'>${movie["title"]}</h5>
+            <span class='rating'>${movie["vote_average"]}</span>
             </div>
         `;
 
